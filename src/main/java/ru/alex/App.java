61ec0bf95c7e9e0689d3359498342b9cd266aba7
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-02 21:52:18 +0300

    replace put on merge in initHashMapFromStr

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -6,9 +6,10 @@ import java.util.Map;
 public class App 
 {
     private static final int COUNT_REPEATS_FOR_UNIQUE = 1;
+    private static final char END_OF_STRING = '\0';
 
     public static void main( String[] args ){
-        System.out.println(getFirstUniqueChar("22331"));
+        System.out.println(getFirstUniqueChar("2331"));
     }
 
     public static Character getFirstUniqueChar(String str) {
@@ -20,23 +21,20 @@ public class App
     }
 
     private static char getFirstUniqueSymbol(String str, Map<Character, Integer> symbolCounts) {
-        char firstUniqueSymbol = '\0';
 
         for (int i = 0; i < str.length(); i++) {
             int countRepeats = symbolCounts.get(str.charAt(i));
             if (countRepeats == COUNT_REPEATS_FOR_UNIQUE) {
-                firstUniqueSymbol = str.charAt(i);
-                break;
+                return str.charAt(i);
             }
         }
 
-        return firstUniqueSymbol;
+        return END_OF_STRING;
     }
 
     private static void initHashMapFromStr(String str, Map<Character, Integer> symbolCounts) {
         for (int i = 0; i < str.length(); i++) {
-            int countRepeats = symbolCounts.getOrDefault(str.charAt(i), 0);
-            symbolCounts.put(str.charAt(i), countRepeats + 1);
+            symbolCounts.merge(str.charAt(i), 1, Integer::sum);
         }
     }
 }

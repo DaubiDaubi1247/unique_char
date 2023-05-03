f43d391b0228c6aac0a449785298a98b40e9a9a9
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-03 09:35:45 +0300

    dev removeDuplicate method

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -1,40 +1,20 @@
 package ru.alex;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
-public class App 
+public class App
 {
-    private static final int COUNT_REPEATS_FOR_UNIQUE = 1;
-    private static final char END_OF_STRING = '\0';
-
     public static void main( String[] args ){
-        System.out.println(getFirstUniqueChar("2331"));
+        int[] intArr = new int[] {1,1,2,3,3,1};
+
+        System.out.println(Arrays.toString(removeDuplicate(intArr)));
     }
 
-    public static Character getFirstUniqueChar(String str) {
-        Map<Character, Integer> symbolCounts = new HashMap<>();
-
-        initHashMapFromStr(str, symbolCounts);
-
-        return getFirstUniqueSymbol(str, symbolCounts);
+    private static int[] removeDuplicate(int[] intArr) {
+        return Arrays.stream(intArr)
+                .distinct()
+                .toArray();
     }
 
-    private static char getFirstUniqueSymbol(String str, Map<Character, Integer> symbolCounts) {
 
-        for (int i = 0; i < str.length(); i++) {
-            int countRepeats = symbolCounts.get(str.charAt(i));
-            if (countRepeats == COUNT_REPEATS_FOR_UNIQUE) {
-                return str.charAt(i);
-            }
-        }
-
-        return END_OF_STRING;
-    }
-
-    private static void initHashMapFromStr(String str, Map<Character, Integer> symbolCounts) {
-        for (int i = 0; i < str.length(); i++) {
-            symbolCounts.merge(str.charAt(i), 1, Integer::sum);
-        }
-    }
 }
